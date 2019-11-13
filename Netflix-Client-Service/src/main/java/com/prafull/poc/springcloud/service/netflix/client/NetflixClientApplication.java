82cd602b71520ec3db574a9bb843e44f1c321f86
Malviya, Prafull <PMALVIA@LordAbbett.com>
2019-11-13 16:52:10 +0530

    New Nteflix Server Module

--- Netflix-Client-Service/src/main/java/com/prafull/poc/springcloud/service/netflix/client/NetflixClientApplication.java
+++ Netflix-Client-Service/src/main/java/com/prafull/poc/springcloud/service/netflix/client/NetflixClientApplication.java
@@ -2,7 +2,9 @@ package com.prafull.poc.springcloud.service.netflix.client;
 
 import com.prafull.poc.springcloud.service.message.MessageServiceApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class NetflixClientApplication extends MessageServiceApplication {
 
     public static void main(String[] args) {
---
+++ Netflix-Server-Service/src/main/java/com/prafull/poc/springcloud/service/netflix/server/NetflixServerApplication.java
@@ -0,0 +1,12 @@
+package com.prafull.poc.springcloud.service.netflix.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixServerApplication.class, args);
+    }
+}

7f50fb46caedb6e98182b3c70480afba22abfc5f
Malviya, Prafull <PMALVIA@LordAbbett.com>
2019-11-18 19:03:49 +0530

    Netflix Cloud POC

--- Netflix-Client-Service/src/main/java/com/prafull/poc/springcloud/service/netflix/client/NetflixClientApplication.java
+++ Netflix-Client-Service/src/main/java/com/prafull/poc/springcloud/service/netflix/client/NetflixClientApplication.java
@@ -3,8 +3,11 @@ package com.prafull.poc.springcloud.service.netflix.client;
 import com.prafull.poc.springcloud.service.message.MessageServiceApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class NetflixClientApplication extends MessageServiceApplication {
 
     public static void main(String[] args) {
--- Netflix-Server-Service/src/main/java/com/prafull/poc/springcloud/service/netflix/server/NetflixServerApplication.java
+++ Netflix-Server-Service/src/main/java/com/prafull/poc/springcloud/service/netflix/server/NetflixServerApplication.java
@@ -2,8 +2,10 @@ package com.prafull.poc.springcloud.service.netflix.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class NetflixServerApplication {
 
     public static void main(String[] args) {
